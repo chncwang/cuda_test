7313bb8c0a3b1a69d4e589540f2e2f55b69684e4
Chauncey Wang <chncwang@gmail.com>
2017-11-24 13:24:24 +0800

    test multivector

--- matrix/src/main.cc
+++ matrix/src/main.cc
@@ -10,25 +10,47 @@
 
 using namespace std;
 
+std::vector<float*> NewGPUVectors(int count, int dim) {
+    std::vector<float*> result;
+    for (int i = 0; i<count; ++i) {
+        float * vec = NewGPUVector(dim);
+        result.push_back(vec);
+    }
+    return result;
+}
+
+void ReleaseGPUVectors(std::vector<float *>* vec) {
+    for (float *v : *vec) {
+        cnmemFree(v, NULL);
+    }
+}
+
 int main() {
-    std::vector<int> dims = {50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000,
-        50000, 100000, 200000, 500000, 1000000};
+    std::vector<int> dims = {50, 100, 200, 500, 1000};
+    std::vector<int> counts = {10, 20, 50, 100, 200, 500, 1000};
     InitGPU(DEVICE::getInstance(), 4000000000, 1);
     cublasHandle_t handle;
     cublasCreate(&handle);
     float alpha = 1.0;
     for (auto dim : dims) {
-        float *gpu_vec_a = NewGPUVector(dim);
-        float *gpu_vec_b = NewGPUVector(dim);
-        cout << "begin cal" << endl;
-        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+        for (auto count : counts) {
+            auto gpu_vec_a = NewGPUVectors(count, dim);
+            auto gpu_vec_b = NewGPUVectors(count, dim);
+            cout << "begin cal" << endl;
+            std::chrono::steady_clock::time_point begin =
+                std::chrono::steady_clock::now();
 
-        for (int i = 0; i < 1000000; ++i) {
-            N3LDGTanh(gpu_vec_a, gpu_vec_b, dim);
+            for (int i = 0; i < 1000000; ++i) {
+            }
+            std::chrono::steady_clock::time_point end =
+                std::chrono::steady_clock::now();
+
+            cout << "dim:" << dim << " count:" << count << " time:" <<
+                std::chrono::duration_cast<std::chrono::microseconds>(end -
+                        begin).count() / 1000<< endl;
         }
-        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-
-        cout << "dim:" << dim << " time:" << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() / 1000<< endl;
-        //PrintGPUVector(gpu_vec_b, 10);
     }
+
+    cudaError_t err = cudaGetLastError();
+    assert(err == cudaSuccess);
 }
