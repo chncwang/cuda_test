5c15255abb5e031bd8f96a71bc567a1d1e2e9157
Chauncey Wang <chncwang@gmail.com>
2017-11-24 16:56:25 +0800

    fix test problem

--- matrix/src/main.cc
+++ matrix/src/main.cc
@@ -1,5 +1,4 @@
 #include "cuda_util.h"
-#include "custom_kernel.cuh"
 #include <cuda_profiler_api.h>
 #include "cnmem.h"
 #include <cassert>
@@ -12,7 +11,7 @@ using namespace std;
 
 int main() {
     std::vector<int> dims = {50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 40000};
-    InitGPU(DEVICE::getInstance(), 4000000000, 0);
+    InitGPU(DEVICE::getInstance(), 6000000000, 1);
     cublasHandle_t handle;
     cublasCreate(&handle);
     float alpha = 1.0;
@@ -20,13 +19,22 @@ int main() {
         float *gpu_vec_a = NewGPUVector(dim);
         float *gpu_vec_b = NewGPUVector(dim);
         cout << "begin cal" << endl;
-        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-
-        for (int i = 0; i < 1000000; ++i) {
+        float sum = 0;
+        int iter = 100000;
+        for (int i = 0; i < iter; ++i) {
+            cudaEvent_t start, stop;
+            cudaEventCreate(&start);
+            cudaEventCreate(&stop);
+            cudaEventRecord(start);
             N3LDGCopyArray(gpu_vec_a, gpu_vec_b, dim);
+            cudaEventRecord(stop);
+            cudaEventSynchronize(stop);
+            float mill;
+            cudaEventElapsedTime(&mill, start, stop);
+            sum += mill;
+            cudaDeviceSynchronize();
         }
-        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
-        cout << "dim:" << dim << " time:" << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() / 1000<< endl;
+        cout << "dim:" << dim << " time:" << sum * 1000 / iter  << endl;
     }
 }
